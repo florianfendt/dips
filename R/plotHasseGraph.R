f2f11250d0260f60d53da4b1a7b99f96ea754ce2
Florian Fendt <flo_fendt@gmx.de>
2018-06-25 12:48:44 +0200

    improve hasse plot

--- R/plotHasseGraph.R
+++ R/plotHasseGraph.R
@@ -5,6 +5,8 @@
 #' @template arg_ps
 #' @param title [\code{character(1L)}]\cr
 #'   Title for the plot. Defaults to empty string, meaning no title.
+#' @param circular [\code{logical(1L)}]\cr
+#'   Should the graph be plotted in circular mode. Defaults to \code{FALSE}.
 #' @param ... \cr
 #'   Additional parameters passed down to \code{igraph::plot.igraph}.
 #'   See \code{\link[igraph]{plot.igraph}} and
@@ -12,26 +14,44 @@
 #'
 #' @return Draws Hasse Graph
 #' @export
-plotHasseGraph = function(ps, title = "", ...) {
+plotHasseGraph = function(ps, title = "", circular = FALSE, ...) {
+  # param checks
   ps = checkPreferenceSystem(ps)
+  assertCharacter(title, len = 1L)
+  assertLogical(circular, len = 1L)
+  # helpers
+  df = ps$df
   r1 = ps$R1
-  a.seq = seq_len(nrow(ps$df))
+  a.seq = seq_len(nrow(df))
   # create graph
   gr = igraph::graph_from_data_frame(r1, directed = TRUE, vertices = a.seq)
   # make tree layout
-  # find best alternatives
-  doms.tab = table(r1[, 1L])
-  root = which(doms.tab == max(doms.tab))
-  layout = igraph::layout_as_tree(gr, root = root, mode = "all")
+  # find alternatives without links first
+  uncomparables = which(rownames(df) %nin% unlist(r1))
+  # subsequently find sugraphs so they get their own tree
+  roots = integer()
+  while (nrow(r1) > 0L) {
+    doms.tab = table(r1[, 1L])
+    root = names(doms.tab)[which(doms.tab == max(doms.tab))]
+    roots = c(roots, root)
+    r1 = r1[r1[, 1L] != root, ]
+  }
+  roots = c(roots, uncomparables)
+  layout = igraph::layout_as_tree(gr, root = roots, mode = "all",
+    circular = circular)
+
   # prepare params
   params = list(x = gr, layout = layout, main = title, ...)
   # add some params to make plot nicer,
   # only if user didn't change them in ...
   if (is.null(params$vertex.label)) {
-    params$vertex.label = paste("a", a.seq, sep = "")
+    params$vertex.label = parse(text = sprintf("a[%s]", a.seq))
   }
   if (is.null(params$vertex.label.dist)) {
-    params$vertex.label.dist = 1
+    params$vertex.label.dist = 2
+  }
+  if (is.null(params$vertex.label.degree)) {
+    params$vertex.label.degree = 0
   }
   if (is.null(params$vertex.label.cex)) {
     params$vertex.label.cex = 0.8
