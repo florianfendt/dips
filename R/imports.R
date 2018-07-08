f81865943f021e99ae88718e1bc33941d86e5868
Florian Fendt <flo_fendt@gmx.de>
2018-07-08 13:10:01 +0200

    Revert "switch back to hasseDiagram"
    This reverts commit 90c97816e2ab4f7eee6422bb2fa28b813a104814.

--- R/imports.R
+++ R/imports.R
@@ -5,7 +5,7 @@
 #' @import utils
 #' @import ggplot2
 #' @import methods
-#' @importFrom hasseDiagram hasse
+#' @importFrom igraph graph_from_data_frame layout_as_tree plot.igraph
 #' @import rcdd
 #' @importFrom Rdpack reprompt
 NULL
--- R/plotHasseGraph.R
+++ R/plotHasseGraph.R
@@ -1,22 +1,86 @@
-#' @title Plots Hasse-Graph from a Preference System
+#' @title Plot Hasse-Graph from a Preference System
 #' @description
 #'   Draws a Hasse Graph for the relation \code{R1} stored in
 #'   the Preference System \code{ps}
 #' @template arg_ps
+#' @param title [\code{character(1L)}]\cr
+#'   Title for the plot. Defaults to empty string, meaning no title.
+#' @param circular [\code{logical(1L)}]\cr
+#'   Should the graph be plotted in circular mode. Defaults to \code{TRUE},
+#'   since this, most of the time, gives the better visualization for
+#'   relations that are not complete.
 #' @param ... \cr
-#'   additional parameters passed down to \code{hasseDiagram::hasse}
+#'   Additional parameters passed down to \code{igraph::plot.igraph}.
+#'   See \code{\link[igraph]{plot.igraph}} and
+#'   \code{\link[igraph]{igraph.plotting}} for details.
+#' @references
+#'   \insertRef{igraph}{dips}
 #' @return Draws Hasse Graph
 #' @export
-plotHasseGraph = function(ps, ...) {
+plotHasseGraph = function(ps, title = "", circular = TRUE, ...) {
+  # param checks
   ps = checkPreferenceSystem(ps)
-  n.levs = nrow(ps$df)
-  levs = seq_len(n.levs)
+  assertCharacter(title, len = 1L)
+  assertLogical(circular, len = 1L)
+  # helpers
+  df = ps$df
   r1 = ps$R1
-  r1.mat = matrix(rep(FALSE, times = n.levs^2), nrow = n.levs, ncol = n.levs)
-  for (i in levs) {
-    x = as.integer(r1[i, ])
-    r1.mat[x[1], x[2]] = TRUE
+  a.seq = seq_len(nrow(df))
+  # create graph
+  gr = igraph::graph_from_data_frame(r1, directed = TRUE, vertices = a.seq)
+  # make tree layout
+  # find alternatives without links first
+  uncomparables = which(rownames(df) %nin% unlist(r1))
+  # subsequently find sugraphs so they get their own tree
+  roots = integer()
+  i = 1L
+  while (nrow(r1) > 0L) {
+    doms.tab = table(r1[, 1L])
+    root = names(doms.tab)[which(doms.tab == max(doms.tab))]
+    if (i == 1L)
+      root = root[1L]
+    dominated = r1[r1[, 1L] %in% root, 2L]
+    r1 = r1[r1[, 1L] %nin% c(root, dominated), ]
+    roots = c(roots, root)
+    i = i + 1L
   }
-  params = list(data = r1.mat, ...)
-  do.call(hasseDiagram::hasse, params)
+  roots = c(roots, uncomparables)
+  layout = igraph::layout_as_tree(gr, root = roots, mode = "all",
+    circular = circular)
+
+  # prepare params
+  params = list(x = gr, layout = layout, main = title, ...)
+  # add some params to make plot nicer,
+  # only if user didn't change them in ...
+  if (is.null(params$vertex.label)) {
+    params$vertex.label = parse(text = sprintf("a[%s]", a.seq))
+  }
+  if (is.null(params$vertex.label.dist)) {
+    params$vertex.label.dist = 2
+  }
+  if (is.null(params$vertex.label.degree)) {
+    params$vertex.label.degree = 0
+  }
+  if (is.null(params$vertex.label.cex)) {
+    params$vertex.label.cex = 0.8
+  }
+  if (is.null(params$vertex.label.color)) {
+    params$vertex.label.color = "black"
+  }
+  if (is.null(params$vertex.size)) {
+    params$vertex.size = 5
+  }
+  if (is.null(params$vertex.color)) {
+    params$vertex.color = "black"
+  }
+  if (is.null(params$edge.arrow.width)) {
+    params$edge.arrow.width = 0.8
+  }
+  if (is.null(params$edge.arrow.size)) {
+    params$edge.arrow.size = 0.5
+  }
+  if (is.null(params$edge.color)) {
+    params$edge.color = "black"
+  }
+  do.call(igraph::plot.igraph, params)
 }
